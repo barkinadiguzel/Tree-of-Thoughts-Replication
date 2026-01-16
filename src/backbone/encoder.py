7b371a0a95a9c75fe927c863b78c618e7fcb2982
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-01-16 20:56:55 +0300

    Update encoder.py

--- src/backbone/encoder.py
+++ src/backbone/encoder.py
@@ -2,12 +2,11 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-class ImageEncoder(nn.Module):
-    def __init__(self, output_dim=512, backbone='resnet18'):
+class Encoder(nn.Module):
+    def __init__(self, output_dim=128):
         super().__init__()
-        if backbone == 'resnet18':
-            self.model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
-            self.model.fc = nn.Linear(self.model.fc.in_features, output_dim)
-
+        self.cnn = models.resnet18(weights=None)  
+        self.cnn.fc = nn.Linear(self.cnn.fc.in_features, output_dim)
+        
     def forward(self, x):
-        return self.model(x) 
+        return self.cnn(x)  
